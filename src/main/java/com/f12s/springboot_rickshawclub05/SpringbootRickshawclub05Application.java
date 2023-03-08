583b91ee41e0bd1f3288cbda9658a8457622488b
JonFriend <jonfriend@JonFriends-MBP.localdomain>
2023-03-08 12:18:12 -0600

    baseline springboot app

---
+++ src/main/java/com/f12s/springboot_rickshawclub05/SpringbootRickshawclub05Application.java
@@ -0,0 +1,13 @@
+package com.f12s.springboot_rickshawclub05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRickshawclub05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRickshawclub05Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/f12s/springboot_rickshawclub05/SpringbootRickshawclub05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f12s.springboot_rickshawclub05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRickshawclub05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
